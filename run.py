5b56e87983c7dfce23b5e050a5c6b13591d1c88c
허민 <heomin02@naver.com>
2024-12-06 15:46:02 +0900

    Del: 서버 기능 제외

--- run.py
+++ run.py
@@ -1,25 +1,16 @@
 from subprocess import Popen, run
 from flask import Flask, request, jsonify
+from Flask.server import process_json
 from scheduling.loadToDB import process_tasks
 import threading
 from scheduling.scheduler import schedule_runner
 
 app = Flask(__name__)
 
+# POST 요청을 처리하는 엔드포인트 설정
 @app.route('/process', methods=['POST'])
-def process_json():
-    # JSON 파일 수신
-    data = request.json
-    reader_test_id = data.get('id')
-    type = data.get('type')
-    if not reader_test_id or not type:
-        return jsonify({"error": "Missing ReaderTest ID or Type"}), 400
-    # 작업 처리 호출
-    result = process_tasks(reader_test_id, type)
-    if result.get('error'):
-        return jsonify(result), 400
-
-    return jsonify({"message": "Tasks added to queue"}), 200
+def handle_process():
+    return process_json()
 
 if __name__ == '__main__':
     # 스케줄러 스레드 실행
