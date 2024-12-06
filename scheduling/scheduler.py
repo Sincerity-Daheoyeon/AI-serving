58d456e295885442d0b019aa62b7fe8b87fd82f8
허민 <heomin02@naver.com>
2024-12-06 15:53:34 +0900

    Add: import model 추가

--- scheduling/scheduler.py
+++ scheduling/scheduler.py
@@ -1,6 +1,8 @@
 from flask import app
 import time
 import AI.init
+from model.model import Model
+
 model = Model(
         input_shape_structured=(3,),
         input_shape_unstructured=(None, 128, 128, 1),
